32a5a34efce6ee3084cb1659ef4163422319010b
Hubert Yu <hubert12161999@gmail.com>
2022-11-15 16:49:32 +0000

    Add explanaintion comments to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#all"
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new" # route to display task form
+  post "tasks", to: "tasks#create" # handle the POST request generated when submitting new task form
   get "tasks/:id", to: "tasks#index", as: :task
   get "tasks/:id/edit", to: "tasks#edit"
   patch "tasks/:id", to: "tasks#update"
